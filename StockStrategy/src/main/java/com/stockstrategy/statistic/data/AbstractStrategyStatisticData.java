bfe8a39f035851c4aa39f07a63608a2241a54071
dave dai <djx0126@126.com>
2017-04-09 22:42:35 +0800

    remove useless

--- StockStrategy/src/main/java/com/stockstrategy/statistic/data/AbstractStrategyStatisticData.java
+++ StockStrategy/src/main/java/com/stockstrategy/statistic/data/AbstractStrategyStatisticData.java
@@ -102,9 +102,6 @@ public  abstract class AbstractStrategyStatisticData extends AbstractStatisticDa
 						int k = i;
 						for (int j = 1;j<6;j++){
 							k = i-j;
-							//if (buysellArray.getValue(k)==-1){
-							//	break;
-							//}
 							if (k >=0 && k < buysellArray.size()){
 								buysellArray.setValue(k, 0);
 							}
