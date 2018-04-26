d58d977923bf3eec9a834ca6f17beb065bf51287
dave dai <djx0126@126.com>
2018-04-27 00:16:07 +0800

    show transaction result dialog in find result page

--- StockStrategyAnalyzerWeb/app/job-result/findtobuy/job-result-findtobuy.js
+++ StockStrategyAnalyzerWeb/app/job-result/findtobuy/job-result-findtobuy.js
@@ -18,16 +18,40 @@
 
 	angular.module('main-module').controller('job-result-findtobuy-controller', JobResultFindtobuyController);
 	
-JobResultFindtobuyController.$inject = ['$scope', '$http', '$timeout', 'jobFactory', 'jobMetaFactory'];
+JobResultFindtobuyController.$inject = ['$scope', '$http', '$timeout', 'jobFactory', 'jobMetaFactory', 'transactionsFactory'];
 	
-function JobResultFindtobuyController($scope, $http, $timeout, jobFactory, jobMetaFactory){
+function JobResultFindtobuyController($scope, $http, $timeout, jobFactory, jobMetaFactory, transactionsFactory){
 	var vm = this;
 	
 	
 	vm.result = $scope.result;
 	vm.resultFindToBuy = [];
 	vm.toggleExpandResult = toggleExpandResult;
-	
+	vm.showTransactionDialog = function (strategy, index) {
+        var resultItem = {
+            strategy: strategy
+        };
+        loadRecentJobResult().then(function (jobResult) {
+			if (jobResult && jobResult.jobId && jobResult.strategyResultMap[strategy]) {
+                transactionsFactory.showTransactionDialog(jobResult.jobId, resultItem, index, vm.resultFindToBuy)
+			}
+        });
+
+        function loadRecentJobResult(){
+            return jobFactory.getJobList().then(function(jobs){
+            	var descJobs = _.sortBy(jobs, function (job) {
+					return -job.id;
+                });
+
+            	var recentJob = _.find(descJobs, function (job) {
+					return job.state ==2 && job.type == 3;
+                });
+                if (recentJob){
+                    return jobFactory.getJobResult(recentJob);
+                }
+            });
+        }
+    };
 	initResults();
 	
 function initResults(){
